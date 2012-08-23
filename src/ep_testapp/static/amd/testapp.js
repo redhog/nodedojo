8f1a7557c1702540633e696d95e8de5fed33dba9
Egil Moeller <egil.moller@freecode.no>
2012-08-23 21:33:42 +0200

    Got dojo to load on client side

--- src/ep_testapp/static/amd/testapp.js
+++ src/ep_testapp/static/amd/testapp.js
@@ -1,8 +1,17 @@
 define(["eh_plugin/static/amd/hooks", "eh_underscore/static/amd/underscore"], function (hooks, _) {
   return {
     create: function (hook_name, args, cb) {
-      console.log("Testapp loaded");
-      cb();
+      dojoConfig = {
+        isDebug: true
+      };
+
+      require(["eh_dojo/static/amd/dojo/main"], function (dojo) {
+        dojo.isBrowser = true;
+        dojo.locale = navigator.language;
+
+        console.log("Testapp loaded");
+        cb();
+      });
     }
   }
 });
